a6dee40ed4baa55ca0373e8be64795a28855d506
Alasdair <ahurst@appcelerator.com>
2016-10-11 20:33:26 +0100

    added dataservice

---
+++ ELBv2/Assets/Scripts/ELB/Data/ELBDataService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ELB.Data {
+	
+	class ELBDataService : Engine.Data.DataService {
+		private const string dbFile = "db.s3db";
+
+		public ELBDataService() : base(dbFile) {}
+
+	};
+}
