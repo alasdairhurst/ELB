2c51f8f32c871fa8efab325114e9e87b39690290
Alasdair <ahurst@appcelerator.com>
2016-10-17 23:12:46 +0100

    some changes

---
+++ ELBv2/Assets/Scripts/Editor/Scene/SceneLoad.cs
@@ -0,0 +1,19 @@
+﻿using ELB.Data.Models;
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class LatestScenes {
+	private static string currentScene;
+	static LatestScenes() {
+		currentScene = EditorApplication.currentScene;
+		EditorApplication.hierarchyWindowChanged += hierarchyWindowChanged;
+	}
+	private static void hierarchyWindowChanged() {
+		if (currentScene != EditorApplication.currentScene) {
+			//a scene change has happened
+			Debug.Log("Last Scene: " + currentScene);
+			currentScene = EditorApplication.currentScene;
+		}
+	}
+}
