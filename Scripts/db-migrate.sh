e6bebfa6fad49165a53c7d141b7b7a98bc3e5421
Alasdair Hurst <omega@omegahurst.co.uk>
2017-04-03 21:01:34 +0100

    update script to only backup if database exists

--- Scripts/db-migrate.sh
+++ Scripts/db-migrate.sh
@@ -1,6 +1,7 @@
-echo "Backing up existing database";
-cp ../ELBv2/Assets/StreamingAssets/db.s3db ../ELBv2/Assets/StreamingAssets/db.bak.s3db
+if [ -f ../ELBv2/Assets/StreamingAssets/db.s3db ]; then
+	echo "Backing up existing database";
+	mv ../ELBv2/Assets/StreamingAssets/db.s3db ../ELBv2/Assets/StreamingAssets/bak/db.s3db.`date +"%s.%3N"`
+fi
 echo "Importing database";
-rm ../ELBv2/Assets/StreamingAssets/db.s3db
 sqlite3 ../ELBv2/Assets/StreamingAssets/db.s3db < ../ELBv2/DBDump/db.s3db.dump
 echo "[Imported database]";
