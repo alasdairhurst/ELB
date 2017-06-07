6ba329283430454c61a888dd146fcb044ef82506
Alasdair Hurst <omega@omegahurst.co.uk>
2017-06-08 00:34:56 +0100

    removed a bunch of unused stuff

--- Scripts/db-dump.sh
+++
@@ -1,4 +0,0 @@
-echo Generating database dump...;
-sqlite3 ../ELBv2/Assets/StreamingAssets/db.s3db .dump > ../ELBv2/DBDump/db.s3db.dump
-echo Generated database dump;
-git add ../ELBv2/DBDump/db.s3db.dump
--- Scripts/db-migrate.sh
+++
@@ -1,7 +0,0 @@
-if [ -f ../ELBv2/Assets/StreamingAssets/db.s3db ]; then
-	echo "Backing up existing database";
-	mv ../ELBv2/Assets/StreamingAssets/db.s3db ../ELBv2/Assets/StreamingAssets/bak/db.s3db.`date +"%s.%3N"`
-fi
-echo "Importing database";
-sqlite3 ../ELBv2/Assets/StreamingAssets/db.s3db < ../ELBv2/DBDump/db.s3db.dump
-echo "[Imported database]";
